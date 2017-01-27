6eb7c11c7c41a7baf65129cfa3be3f126c78e624
cscherbert1 <cscherbert89@gmail.com>
2017-01-26 21:04:10 -0600

    Added a variable and changed the message

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -15,7 +15,9 @@ public class FirstGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Java!");
+        
+        int x = 5;
     }
     
 }
